b2867ec8079e38d770e0a9c7592777b33f76d1a2
Caio <caio@MacCris.local>
2018-02-12 20:41:04 -0500

    Finished

---
+++ Lesson3/HexSqrt.py
@@ -0,0 +1,4 @@
+import math
+sL = float(input("Enter the side length: "))
+area = (3 * math.sqrt(3) / 2) * sL**2
+print(area)
---
+++ Lesson3/areaNperimeter2.py
@@ -0,0 +1,6 @@
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+perimeter = length * 2 + width * 2
+area = width * length
+print("The area is: %1.0f" % area)
+print("The perimeter is: %1.0f" % perimeter)
